24f21856d2fe5ad95af7cc6b6ce5e1d5317c52e0
MrBaldman <gpharwood@gmail.com>
2013-09-20 14:56:24 +0100

    working filters and updates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,19 +3,27 @@
 /* Controllers */
 
 angular.module('sk8.controllers', []).
-  controller('updatesController', [function($scope) {
+  controller('updatesController', function($scope) {
   		$scope.updateTypes = [
   			'recent',
   			'nearby',
-  			'popular'
+  			'additions'
   		];
   		$scope.updates = [
   			{
   				name : 'New Skatepark',
-  				updateType : 
+  				updateType : 'additions'
+  			},
+  			{
+  				name : 'Updated Skatepark',
+  				updateType : 'recent'
+  			},
+  			{
+  				name : 'Close Skatepark',
+  				updateType : 'nearby'
   			}
   		];
-  }])
-  .controller('mapController', [function() {
+  })
+  .controller('mapController', function() {
 
-  }]);
+  });
